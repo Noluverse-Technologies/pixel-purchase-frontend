3c033798dc33140b33f98b05519b247096e5d3c0
soumyaRauth <shoummorauth@live.co.uk>
2022-03-28 03:33:30 +0600

    git cache cleared from next branch

--- variables/config.js
+++
@@ -1,5 +0,0 @@
-export const authToken = typeof window !=="undefined"?localStorage.getItem("authToken"):null;
-export const baseUrl="http://localhost:3000/api/";
-export const rootUrl="http://localhost:3000/";
-export const noluPlusPackageId=1;
-
